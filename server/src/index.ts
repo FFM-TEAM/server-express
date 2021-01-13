ca686a6021dc3e03a7e3b221094e28684b9bb7ae
s-ong-c <somony9292@gmail.com>
2021-01-13 22:05:31 +0900

    feat: mikroORM setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log('log');
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+   await orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: "test post" });
+  // await orm.em.persistAndFlush(post);
+  // console.log("__________________________sql__________");
+  // await orm.em.nativeInsert(Post, { title: "my first post 2" });
+  // const post = new Post('my first post')
+};
+
+main().catch((err) => {
+  console.log(err);
+});
