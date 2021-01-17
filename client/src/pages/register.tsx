2eb1dfe5a8a36baf20b78065f4bf8fbccd46a9a8
s-ong-c <somony9292@gmail.com>
2021-01-17 19:19:42 +0900

    feat: [client] Register Error Handling

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -4,21 +4,29 @@ import { Form, Formik } from "formik";
 import InputField from "../components/InputField";
 import React from "react";
 import Wrapper from "../components/Wrapper";
+import { toErrorMap } from "../utils/toErrorMap";
 import { useMutation } from "urql";
 import { useRegisterMutation } from "../generated/graphql";
+import { useRouter } from "next/router";
 
 interface Props {}
 
 function Register(props: Props) {
-  const [, register] = useRegisterMutation()
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
 
   return (
     <Wrapper>
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
-          response.data.register?.user?.id
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            // worked
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
